d0b1a7331f27695b2d1952bb8240c3ee2330560f
TanishkM <stanishkmehta5k@gmail.com>
2022-08-07 19:37:10 +0530

    Transations display

---
+++ src/components/Adds.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const Adds = ({data}) => {
+
+    return (
+        <div>
+            <div className=' mt-4'>
+                <div className='d-flex justify-content-between'>
+                    <div>
+                        <div className='d-inline px-3 transaction'>
+                            {data.category}
+                        </div>
+                        <div className='d-inline px-3 transaction'>
+                            <b>{data.title} </b>
+
+                        </div>
+
+                    </div>
+                    <div className='d-flex align-items-center'>
+                        <div className='d-inline  transaction' style={{color:data.transaction<0?"red":"#06ad00"}}>
+                            <b>₹ {Math.abs(data.transaction)}</b>
+
+                        </div>
+                        <div className='d-inline px-3'>
+                            <button style={{ backgroundColor: "white", border: "none" }}>
+                                <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd"><path d="M19 24h-14c-1.104 0-2-.896-2-2v-16h18v16c0 1.104-.896 2-2 2m-9-14c0-.552-.448-1-1-1s-1 .448-1 1v9c0 .552.448 1 1 1s1-.448 1-1v-9zm6 0c0-.552-.448-1-1-1s-1 .448-1 1v9c0 .552.448 1 1 1s1-.448 1-1v-9zm6-5h-20v-2h6v-1.5c0-.827.673-1.5 1.5-1.5h5c.825 0 1.5.671 1.5 1.5v1.5h6v2zm-12-2h4v-1h-4v1z" /></svg>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr />
+        </div>
+    )
+}
+
+export default Adds
--- src/components/DashBot.js
+++ src/components/DashBot.js
@@ -1,15 +1,41 @@
 import React from 'react'
 import { useSpring, animated } from 'react-spring'
+import Adds from './Adds'
 const DashBot = () => {
   const style1 = useSpring({
     from: { marginTop: 500 },
     to: { marginTop: 0 },
     config: { duration: 500 }
   })
+
+  const datas = [
+    {
+        title: "firstsecond",
+        category: "s",
+        description: "some description",
+        transaction: 10
+    },
+    {
+      title: "thirdfourth",
+      category: "s",
+      description: "some other description",
+      transaction: -100000
+  }
+]
+
   return (
     <animated.div style={style1}>
       <div>
-        <div className='container bottom shadow-lg'></div>
+        <div className='container bottom shadow-lg'>
+        <h4 className='mt-3'>Transactions</h4>
+        {datas.length===0? "No Transaction to display":  
+            datas.map((data)=>{
+                return (<Adds data={data}/>   
+                )
+            })
+              }
+        </div>
+        
       </div>
     </animated.div>
 
