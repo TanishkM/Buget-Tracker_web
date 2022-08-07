56092e436f32db659b81fda6cd5e86160129758a
TanishkM <stanishkmehta5k@gmail.com>
2022-08-07 16:36:20 +0530

    Transitions and styling

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import { useSpring, animated } from 'react-spring'
 
 const Balance = () => {
+  const style1 = useSpring({
+    from: { marginRight: 300},
+    to: { marginRight:0 },
+    config: { duration: 200 }
+  })
   return (
+        <animated.div style={style1}>
     <div>
       <div className='container d-flex align-items-center justify-content-center text-light text-center bal'>
-        <h3 className='d-inline ' style={{fontSize:"4em"}}>₹ 1,42,000</h3>
+        <h3 className='d-inline ' style={{ fontSize: "4em" }}>₹ 1,42,000</h3>
       </div>
     </div>
+    </animated.div>
+    
   )
 }
 
--- src/components/DashBot.js
+++ src/components/DashBot.js
@@ -1,10 +1,18 @@
 import React from 'react'
-
+import { useSpring, animated } from 'react-spring'
 const DashBot = () => {
+  const style1 = useSpring({
+    from: { marginTop: 500 },
+    to: { marginTop: 0 },
+    config: { duration: 500 }
+  })
   return (
-    <div>
-      <div className='container bottom shadow-lg'></div>
-    </div>
+    <animated.div style={style1}>
+      <div>
+        <div className='container bottom shadow-lg'></div>
+      </div>
+    </animated.div>
+
   )
 }
 
