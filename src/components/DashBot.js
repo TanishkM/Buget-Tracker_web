f01359d6c0a120e7a366f1bce46b55d9ee9527bd
TanishkM <stanishkmehta5k@gmail.com>
2022-08-08 03:29:37 +0530

    scroll component and styling

--- src/components/DashBot.js
+++ src/components/DashBot.js
@@ -13,13 +13,15 @@ const DashBot = ({datas,getTransactions,loading}) => {
       <div>
         <div className='container bottom shadow-lg'>
           <div className='mt-3 d-flex justify-content-between'>
-          <h4 className='mt-1 d-inline'>Transactions</h4>
+          <h4 className='mt-1 d-inline '>Transactions</h4>
           <div className='addbtn'>
           <Link to="/add">
           <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-5v5h-2v-5h-5v-2h5v-5h2v5h5v2z"/></svg>
           </Link>
         </div>
           </div>
+          <div className='scroll'>
+
           {loading?<div className="spinner-border text-primary" role="status">
             <span className="visually-hidden">Loading...</span>
           </div>:""}
@@ -29,6 +31,7 @@ const DashBot = ({datas,getTransactions,loading}) => {
               )
             })
           }
+          </div>
         </div>
       </div>
     </animated.div>
