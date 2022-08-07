13d4b99ec964ca59fac9806479d061a25f6ea0af
TanishkM <stanishkmehta5k@gmail.com>
2022-08-07 16:15:49 +0530

    Dashboard Upper

---
+++ src/components/Balance.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Balance = () => {
+  return (
+    <div>
+      <div className='container d-flex align-items-center justify-content-center text-light text-center bal'>
+        <h3 className='d-inline' style={{fontSize:"4em"}}>₹ 1,42,000</h3>
+      </div>
+    </div>
+  )
+}
+
+export default Balance
---
+++ src/components/DashBot.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DashBot = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default DashBot
