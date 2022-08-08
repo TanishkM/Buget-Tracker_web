3ab13239b30bb6ea7c773162513086d2972991bf
TanishkM <stanishkmehta5k@gmail.com>
2022-08-09 01:42:55 +0530

    test

--- src/fire.js
+++ src/fire.js
@@ -2,12 +2,12 @@ import {initializeApp} from 'firebase/app'
 import {getAuth} from 'firebase/auth'
 import {getFirestore} from 'firebase/firestore'
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: "AIzaSyCP37xXp2zL25cdzv4ZNoktWOSBptZnNo0",
+  authDomain: "login-97d25.firebaseapp.com",
+  projectId: "login-97d25",
+  storageBucket: "login-97d25.appspot.com",
+  messagingSenderId: "899847717708",
+  appId: "1:899847717708:web:8e88660469a510e465c5e9"
   };
   export const app = initializeApp(firebaseConfig);
   export const auth=getAuth(app)
