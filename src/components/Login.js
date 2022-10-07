c765fef42b883bfdeeba701fd060488345dafd35
sahanmndl <mondalsahan053@gmail.com>
2022-10-08 02:02:19 +0530

    Show errors in login and sign up page

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,6 +6,8 @@ import cntx from '../context/cntx'
 const Login = () => {
     const navigate=useNavigate()
     const [loading,setLoading]=useState(false)
+    const [error, setError] = useState(false)
+    const [errorMessage, setErrorMessage] = useState("")
     const a=useContext(cntx)
     const emailRef = useRef()
     const passRef = useRef()
@@ -16,17 +18,25 @@ const Login = () => {
     })
 
     const handleLogin=async()=>{
-        try
-        {
-            setLoading(true)
-            await a.login(emailRef.current.value,passRef.current.value)
-            navigate('/')
+        if(emailRef.current.value.trim() === "" || passRef.current.value.trim() === "") {
+            setError(true)
+            setErrorMessage("Inputs cannot be empty")
+        } else if (passRef.current.value.trim().length < 8) {
+            setError(true)
+            setErrorMessage("Password must be atleast 8 characters long")
+        } else {
+            setError(false)
+            try
+            {
+                setLoading(true)
+                await a.login(emailRef.current.value,passRef.current.value)
+                navigate('/')
+            }
+            catch(err){
+                alert(err.message)
+            }
+            setLoading(false)
         }
-            
-        catch(err){
-            alert(err.message)
-        }
-        setLoading(false)
     }
     return (
         <animated.div style={style1}>
@@ -43,6 +53,11 @@ const Login = () => {
                             <label htmlFor="floatingPassword">Password</label>
                         </div>
                         <button disabled={loading} onClick={handleLogin} className="btn btn-primary d-block w-100 mt-4 btn-lg" type="button">Sign In</button>
+                        {error && 
+                            <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', color: 'red', marginTop: 2}}>
+                                <text>{errorMessage}</text>
+                            </div>
+                        }
                         <Link className="link-primary text-center d-block mt-2" to="/sign_up">Create a New Account?</Link>
                     </div>
                 </div>
